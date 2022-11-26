74b33621e2279d69fd2b3def2af654c0247171ac
Miko <jm_mapilisan@yahoo.com>
2022-11-26 20:55:42 +0800

    add prefetch

--- src/features/auth/Prefetch.jsx
+++ src/features/auth/Prefetch.jsx
@@ -5,16 +5,12 @@ import { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 
 const Prefetch = () => {
-  useEffect(() => {
-    const notes = store.dispatch(notesApiSlice.endpoints.getNotes.initiate())
-    const users = store.dispatch(usersApiSlice.endpoints.getUsers.initiate())
+	
+	useEffect(() => {
+		store.dispatch(notesApiSlice.util.prefetch('getNotes', 'notesList', { force: true }))
+		store.dispatch(usersApiSlice.util.prefetch('getUsers', 'usersList', { force: true }))
+	}, [])
 
-    return () => {
-      notes.unsubscribe()
-      users.unsubscribe()
-    }
-  }, [])
-
-  return <Outlet />
+	return <Outlet />
 }
 export default Prefetch
