972acf72615da4538f33a9b9af6c51aed4f1f36a
Miko <jm_mapilisan@yahoo.com>
2022-11-26 21:00:41 +0800

    add only admins and managers can view all notes

--- src/features/notes/NotesList.jsx
+++ src/features/notes/NotesList.jsx
@@ -1,49 +1,61 @@
 import Note from "./Note";
 import { useGetNotesQuery } from "./notesApiSlice"
+import useAuth from "../../hooks/useAuth"
+import PulseLoader from "react-spinners/PulseLoader";
 
 const NotesList = () => {
-  const {
-    data: notes,
-    isLoading,
-    isSuccess,
-    isError,
-    error
-  } = useGetNotesQuery(undefined, {
-    pollingInterval: 15000,
-    refetchOnFocus: true,
-    refetchOnMountOrArgChange: true
-  })
 
-  let content;
+	const { username, isManager, isAdmin } = useAuth()
 
-  if (isLoading) content = <p>Loading...</p> 
-  if (isError) content = <p className={isError ? 'errmsg' : 'offscreen'}>{error?.data?.message}</p>
+	const {
+		data: notes,
+		isLoading,
+		isSuccess,
+		isError,
+		error
+	} = useGetNotesQuery('notesList', {
+		pollingInterval: 15000,
+		refetchOnFocus: true,
+		refetchOnMountOrArgChange: true
+	})
 
-  if (isSuccess) {
-    const { ids } = notes
+	let content;
 
-    const tableContent = ids?.length ? ids.map(noteId => <Note key={noteId} noteId={noteId} />) : null
+	if (isLoading) content = <PulseLoader color={ '#FFF' } /> 
+	if (isError) content = <p className={isError ? 'errmsg' : 'offscreen'}>{error?.data?.message}</p>
 
-    content = (
-      <table className='table table--notes'>
-        <thead className='table__thead'>
-          <tr>
-            <th scope='col' className='table__th note__status'>Status</th>
-            <th scope='col' className='table__th note__created'>Created</th>
-            <th scope='col' className='table__th note__updated'>Updated</th>
-            <th scope='col' className='table__th note__title'>Title</th>
-            <th scope='col' className='table__th note__username'>Owner</th>
-            <th scope='col' className='table__th note__edit'>Edit</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tableContent}
-        </tbody>
-      </table>
-    )
-  }
+	if (isSuccess) {
+		const { ids, entities } = notes
 
-  return content
+		let filteredIds
+		if (isManager || isAdmin) {
+			filteredIds = [...ids]
+		} else {
+			filteredIds = ids.filter(noteId => entities[noteId].username === username ) 
+		}
+
+		const tableContent = ids?.length && filteredIds.map(noteId => <Note key={noteId} noteId={noteId} />) 
+
+		content = (
+		<table className='table table--notes'>
+			<thead className='table__thead'>
+			<tr>
+				<th scope='col' className='table__th note__status'>Status</th>
+				<th scope='col' className='table__th note__created'>Created</th>
+				<th scope='col' className='table__th note__updated'>Updated</th>
+				<th scope='col' className='table__th note__title'>Title</th>
+				<th scope='col' className='table__th note__username'>Owner</th>
+				<th scope='col' className='table__th note__edit'>Edit</th>
+			</tr>
+			</thead>
+			<tbody>
+			{tableContent}
+			</tbody>
+		</table>
+		)
+	}
+
+	return content
 }
 
 export default NotesList
