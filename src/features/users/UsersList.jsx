6071556fb40de14d3bee069b49ea78604ad2d84d
Miko <jm_mapilisan@yahoo.com>
2022-11-26 21:01:59 +0800

    refactor users list

--- src/features/users/UsersList.jsx
+++ src/features/users/UsersList.jsx
@@ -1,3 +1,4 @@
+import PulseLoader from "react-spinners/PulseLoader";
 import User from "./User";
 import { useGetUsersQuery } from "./usersApiSlice"
 
@@ -8,7 +9,7 @@ const UsersList = () => {
     isSuccess,
     isError,
     error
-  } = useGetUsersQuery(null, {
+  } = useGetUsersQuery('usersList', {
     pollingInterval: 60000,
     refetchOnFocus: true,
     refetchOnMountOrArgChange: true
@@ -16,13 +17,13 @@ const UsersList = () => {
 
   let content;
 
-  if (isLoading) content = <p>Loading...</p>
+  if (isLoading) content = <PulseLoader color={ '#FFF' } />
   if (isError) content = <p className={isError ? "errmsg" : "offscreen"}>{error?.data?.message}</p>
   
   if (isSuccess) {
     const { ids } = users
 
-    const tableContent = ids?.length ? ids.map(userId => <User key={userId} userId={userId} />) : null
+    const tableContent = ids?.length && ids.map(userId => <User key={userId} userId={userId} />)
 
     content = (
       <table className='table table--users'>
